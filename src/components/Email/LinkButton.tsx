403368bdb58475e2be0c7255117683b269ae6ae1
andrew keh <andy.keh@gmail.com>
2023-07-08 15:18:05 +0800

    link styling

--- src/components/Email/LinkButton.tsx
+++ src/components/Email/LinkButton.tsx
@@ -8,7 +8,7 @@ const LinkButton = (props: React.ButtonHTMLAttributes<HTMLButtonElement>) => (
       textTransform: "none",
       padding: 0,
       minWidth: 0,
-      color: "#18216d", // Change this to the desired link color
+      color: "#18216d", // LINK COLOR
       background: "none",
       border: "none",
       cursor: "pointer",
